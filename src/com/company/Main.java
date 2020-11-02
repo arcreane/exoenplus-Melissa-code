43ee918aa074523e0e9764835ed2ec7238e5c5d0
Melissa BENARD <melissabenard@MacBook-Pro-de-Melissa.local>
2020-11-02 15:20:04 +0100

    premier commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner Scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+	// write your code here
+        // Afficher le menu
+        // Gérer la saisie de l'utilisateur
+
+        do {
+            // afficheMenu();
+            // gereSaisieUtilisateur();
+        } while // !Quit
+    }
+
+    private static
+
+}
